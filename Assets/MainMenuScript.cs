520443b0841e669bf1319506cd355859f2d4f772
GanzaMos <moskalik.ganza@gmail.com>
2020-06-02 19:24:48 +0300

    Add a music into main menu and script to play it in other levels

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("StartNewGame", 3);
+
+    }
+
+    private void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+        Object.DontDestroyOnLoad(GameObject.Find("Music Player"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
