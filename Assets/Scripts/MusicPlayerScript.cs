74ee8bc98ab4ef3620d3c0dcd89ca49332841741
GanzaMos <moskalik.ganza@gmail.com>
2020-06-17 21:26:44 +0300

    Orginize some staff in project

--- Assets/Scripts/MusicPlayerScript.cs
+++ Assets/Scripts/MusicPlayerScript.cs
@@ -3,18 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuScript : MonoBehaviour
+public class MusicPlayerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("StartNewGame", 3);
-
-    }
-
-    private void StartNewGame()
-    {
-        SceneManager.LoadScene(1);
         Object.DontDestroyOnLoad(GameObject.Find("Music Player"));
     }
 
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("StartNewGame", 3);
+    }
+
+    private void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
