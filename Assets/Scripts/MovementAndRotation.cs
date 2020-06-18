d3e4193fd73f9ef5a7a0488cc6bf2b287ae2ff53
GanzaMos <moskalik.ganza@gmail.com>
2020-06-18 14:02:36 +0300

    Movent freeze when collision happens

--- Assets/Scripts/MovementAndRotation.cs
+++ Assets/Scripts/MovementAndRotation.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour
+public class MovementAndRotation : MonoBehaviour
 {
-
+    [Header("Offset speed")]
     [Tooltip("ms^-1")] [SerializeField] float xSpeed = 150f;
     [Tooltip("ms^-1")] [SerializeField] float ySpeed = 150f;
+
+
+    [Header("Limits available screen to movement")]
     [SerializeField] float xMaxOffset = 30f;
     [SerializeField] float yMaxOffset = 15f;
 
+    [Header("Something about moving offset")]
     [SerializeField] float pitchOffset = 10f;
     [SerializeField] float yawOffset = 10f;
 
+    [Header("Offset when moving")]
     [SerializeField] float pitchOffsetMoment = 10f;
     [SerializeField] float yawOffsetMoment = 30f;
     [SerializeField] float rollOffsetMoment = 30f;
@@ -21,6 +26,7 @@ public class Player : MonoBehaviour
 
 
     float xThrow, yThrow;
+    bool controlEnable = true;
 
     // Start is called before the first frame update
     void Start()
@@ -31,9 +37,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessTranslation();
-        ProcessRotation();
+        if (controlEnable == true)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+    }
 
+    void OnPlayerDeath() //called by string referance. Don't rename!
+    {
+        controlEnable = false;
+        print("Movement frozen");
     }
 
     private void ProcessRotation()
@@ -62,11 +76,6 @@ public class Player : MonoBehaviour
             Mathf.Clamp(transform.localPosition.y + yOffset, -yMaxOffset, yMaxOffset),
             transform.localPosition.z
             );
-    }
-
-    private void OnTriggerEnter (Collider collision)
-    {
-        print("Trigger collision");
     }
 
 }
